ee52ebe0ff9f791f563d25f8f2d9c9db6cc3c229
webermg <matthew.gerald.weber@gmail.com>
2020-09-18 19:25:39 -0700

    animation tweaks and prevented button from displaying on final page

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # anniversary-card
 
 get the background right -done
-create empty foreground card
+create empty foreground card -done
+create card header -done
+create card button -done
+setup display of card states -done
+acquire fonts and display -done
 animate card states
+    set card button to hide until animations are done
+set media queries
+maybe get image for card button
+
 
 state 1: pic floated left, text
 state 2: pic floated right, text
 state 3: pic floated left, text
+etc
--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //card state info
 const cardStates = [
     {
-        src: "assets/test1.jpeg",
+        src: "assets/photo1.jpg",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tincidunt ante ut tortor consectetur tincidunt. Morbi finibus nisi vel mi dignissim, eget dapibus tellus molestie. Nulla rhoncus sem sit amet vulputate pellentesque. Donec aliquet libero condimentum quam mattis, sit amet tempor tortor suscipit. Quisque eleifend risus a eros pulvinar, ac hendrerit lorem congue. Nunc libero orci, finibus sed interdum et, fringilla quis ex. Morbi tristique, lacus eget pellentesque viverra, dui elit porttitor ligula, quis sollicitudin lacus nunc eget nibh. Cras feugiat hendrerit eros, in lobortis enim dignissim non. Cras et molestie diam, in lobortis tortor. Morbi non orci luctus urna blandit consequat vel ac lacus."
     },
     {
-        src: "assets/test2.jpeg",
+        src: "assets/photo2.jpg",
         text: "In neque eros, ornare nec arcu non, tempor consectetur eros. Proin tempor felis tellus, et porta massa malesuada vitae. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla consectetur arcu sed libero placerat, at commodo massa sollicitudin. Aenean eleifend tortor nisl, id fermentum ante tincidunt non. Mauris nec eleifend metus. Vivamus tellus purus, ullamcorper sed ex lobortis, venenatis tincidunt libero. Nam ultrices est quam, vitae hendrerit magna fermentum ornare. Nam consequat tristique sapien, vel vulputate diam iaculis eu. Vestibulum a tellus consequat elit maximus sodales at id metus. Etiam ut lacus sed lectus convallis tristique."
     },
     {
-        src: "assets/test3.jpg",
+        src: "assets/photo3.jpg",
         text: "Mauris suscipit sapien diam, gravida aliquam turpis varius sit amet. Vivamus interdum velit dolor. In hac habitasse platea dictumst. Curabitur eu laoreet nunc. Sed ultrices varius ligula vel fringilla. Aliquam odio dui, vulputate et euismod non, egestas in ligula. Donec convallis laoreet convallis. Phasellus faucibus interdum mollis. Aenean in massa ut odio fermentum luctus a non tellus. Nunc volutpat arcu vel accumsan mollis. Praesent et congue ex. Vivamus sagittis purus ut magna rutrum, sed auctor ipsum molestie. Integer molestie tellus quis molestie laoreet."
     }
 ];
@@ -22,7 +22,9 @@ const next = document.getElementById("next");
 
 const init = () => {
     index = 0;
-    setState(cardStates, index);
+    setTimeout(() => {
+        setState(cardStates, index);
+    }, 1000);
 
 };
 
@@ -40,11 +42,15 @@ const setState = (states,index) => {
     }
     pic.setAttribute("src", states[index].src);
     text.textContent = states[index].text;
-    setTimeout(() => {
-        next.style.opacity = "100%";
-    }, 7000);
-    delay(fadeIn, pic, 3000, 2000);
-    delay(fadeIn, text, 3000, 4000);
+    if(index < states.length-1) {
+        setTimeout(() => {
+            next.style.opacity = "100%";
+            next.style.cursor = "pointer";
+            next.disabled = false;
+        }, 6000);
+    }
+    delay(fadeIn, pic, 3000, 1000);
+    delay(fadeIn, text, 3000, 3000);
     
 };
 
@@ -76,6 +82,8 @@ const fadeOut = (element, duration) => {
 
 next.addEventListener("click", e => {
     //fadeout
+    next.style.cursor = "default";
+    next.disabled = true;
     fadeOut(pic, 2000);
     fadeOut(text, 2000);
     fadeOut(next, 2000);
@@ -83,7 +91,7 @@ next.addEventListener("click", e => {
     index = index+1 < cardStates.length ? index+1 : 0;
     setTimeout(() => {
         setState(cardStates, index);
-    },3000);
+    },2000);
 });
 
 init();
